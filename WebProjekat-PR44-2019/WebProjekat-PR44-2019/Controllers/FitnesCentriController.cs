ab1af16bc4e8bb156bbc9ba9fdc5061ffc9358ef
nemanjaASE <nightwalker44ASE@gmail.com>
2022-06-27 15:50:53 +0200

    Prikaz, sortiranje i filtriranje fitnes centara
    - Omogućen je prikaz fitnes centara na početnoj strani
    - Omogućeno je sortiranje po nazivu, adresi i godini osnivanja
    - Omogućeno je filtriranje po nazivu, adresi i godini
    - Omogućeno je kombinovano filtriranje

---
+++ WebProjekat-PR44-2019/WebProjekat-PR44-2019/Controllers/FitnesCentriController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebProjekat_PR44_2019.Models;
+
+namespace WebProjekat_PR44_2019.Controllers
+{
+    public class FitnesCentriController : ApiController
+    {
+        private static FitnesCentriRepo repo = new FitnesCentriRepo();
+        public IEnumerable<FitnesCentar> Get()
+        {
+            List<FitnesCentar> fitnesCentri = repo.DobaviFitnesCentre();
+
+            int n = fitnesCentri.Count();
+            for (int i = 0; i < n; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (String.Compare(fitnesCentri[j].Naziv, fitnesCentri[min].Naziv) < 0)
+                    {
+                        min = j;
+                    }
+                   
+                }
+                if (min != i)
+                {
+                    var tmp = fitnesCentri[i];
+                    fitnesCentri[i] = fitnesCentri[min];
+                    fitnesCentri[min] = tmp;
+                }
+            }
+            return repo.DobaviFitnesCentre();
+        }
+    }
+}
---
+++ WebProjekat-PR44-2019/WebProjekat-PR44-2019/Models/FitnesCentriRepo.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebProjekat_PR44_2019.Models
+{
+    public class FitnesCentriRepo
+    {
+        private List<FitnesCentar> fitnesCentri;
+        public FitnesCentriRepo()
+        {
+            string path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/fitnesCentri.json");
+            string jsonString = File.ReadAllText(path);
+            var dataModel = JsonConvert.DeserializeObject<List<FitnesCentar>>(jsonString);
+            fitnesCentri = new List<FitnesCentar>();
+
+            foreach (var fitnesCentar in dataModel)
+            {
+                FitnesCentar ft = new FitnesCentar() {
+                    Naziv = fitnesCentar.Naziv,
+                    Adresa = fitnesCentar.Adresa,
+                    CenaJGT = fitnesCentar.CenaJGT,
+                    CenaJT = fitnesCentar.CenaJT,
+                    CenaMC = fitnesCentar.CenaMC,
+                    CenaJTP = fitnesCentar.CenaJTP,
+                    GodinaOtvaranja = fitnesCentar.GodinaOtvaranja,
+                    Vlasnik = fitnesCentar.Vlasnik
+                };
+                fitnesCentri.Add(ft);
+            }
+        }
+
+        public List<FitnesCentar> DobaviFitnesCentre()
+        {
+            return this.fitnesCentri;
+        }
+    }
+}

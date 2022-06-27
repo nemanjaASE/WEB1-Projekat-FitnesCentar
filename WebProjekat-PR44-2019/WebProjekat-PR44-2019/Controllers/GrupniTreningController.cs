f88a5114b75d7bd890106778d7136e8a7af7fde4
nemanjaASE <nightwalker44ASE@gmail.com>
2022-06-27 22:20:50 +0200

    Prikaz detalja o fitnes centru i grupnih treninga
    - Dodata je stranica koja detaljno prikazuje informacije o fitnes centru
    - Na istoj stranici je dodat i tabelaran prikaz budućih grupnih treninga za prikazani fitnes centar

---
+++ WebProjekat-PR44-2019/WebProjekat-PR44-2019/Controllers/GrupniTreningController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebProjekat_PR44_2019.Models;
+
+namespace WebProjekat_PR44_2019.Controllers
+{
+    public class GrupniTreningController : ApiController
+    {
+        private static GrupniTreningRepo repo = new GrupniTreningRepo();
+        public IEnumerable<GrupniTrening> Get(string naziv, string ulica, string broj, string grad)
+        {
+            List<GrupniTrening> grupniTreninzi = repo.DobaviGrupneTreninge();
+            List<GrupniTrening> temp = new List<GrupniTrening>();
+            DateTime now = DateTime.Now;
+            int dan = now.Day;
+            int mesec = now.Month;
+            int godina = now.Year;
+            int sat = now.Hour;
+            int min = now.Minute;
+
+            foreach (var gt in grupniTreninzi)
+            {
+                string[] tokens = gt.DatumIVreme.Split(' ');
+                int tempDan = int.Parse(tokens[0].Split('/')[0]);
+                int tempMesec = int.Parse(tokens[0].Split('/')[1]);
+                int tempGodina = int.Parse(tokens[0].Split('/')[2]);
+
+                int tempSat = int.Parse(tokens[1].Split(':')[0]);
+                int tempMin = int.Parse(tokens[1].Split(':')[1]);
+
+                if (tempGodina < godina)
+                    continue;
+                if (tempMesec < mesec && tempGodina == godina)
+                    continue;
+                if (tempDan < dan && tempGodina == godina && tempMesec == mesec)
+                    continue;
+                if (tempSat < sat && tempDan == dan && tempGodina == godina && tempMesec == mesec)
+                    continue;
+                if (tempMin < min && tempSat == sat && tempDan == dan && tempGodina == godina && tempMesec == mesec)
+                    continue;
+
+                if (gt.FitnesCentarr.Naziv == naziv 
+                    && gt.FitnesCentarr.Adresa.Ulica == ulica
+                    && gt.FitnesCentarr.Adresa.Broj.ToString() == broj
+                    && gt.FitnesCentarr.Adresa.Grad == grad)
+                {
+                    temp.Add(gt);
+                }
+            }
+            return temp;
+        }
+
+        public IEnumerable<GrupniTrening> Get()
+        {
+            return repo.DobaviGrupneTreninge();
+        }
+    }
+}
---
+++ WebProjekat-PR44-2019/WebProjekat-PR44-2019/Models/GrupniTreningRepo.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebProjekat_PR44_2019.Models
+{
+    public class GrupniTreningRepo
+    {
+        private List<GrupniTrening> grupniTreninzi;
+        public GrupniTreningRepo()
+        {
+            string path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/grupniTreninzi.json");
+            string jsonString = File.ReadAllText(path);
+            var dataModel = JsonConvert.DeserializeObject<List<GrupniTrening>>(jsonString);
+            grupniTreninzi = new List<GrupniTrening>();
+
+            List<FitnesCentar> fitnesCentri = new List<FitnesCentar>();
+
+            foreach (var grupniTrening in dataModel)
+            {
+                GrupniTrening gt = new GrupniTrening()
+                {
+                    DatumIVreme = grupniTrening.DatumIVreme,
+                    MaxPosetilaca = grupniTrening.MaxPosetilaca,
+                    Naziv = grupniTrening.Naziv,
+                    Posetioci = grupniTrening.Posetioci,
+                    TrajanjeTreninga = grupniTrening.TrajanjeTreninga,
+                    TipTreninga = grupniTrening.TipTreninga,
+                    FitnesCentarr = grupniTrening.FitnesCentarr
+                };
+                
+                grupniTreninzi.Add(gt);
+            }
+        }
+
+        public List<GrupniTrening> DobaviGrupneTreninge()
+        {
+            return this.grupniTreninzi;
+        }
+    }
+}
